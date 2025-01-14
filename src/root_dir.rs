1da693f1e5d06620c4cd4e39c9329eef967e1e12
Lorenzo Ferranti <lorenzoferranti04@outlook.it>
2025-01-14 22:04:46 +0100

    helper functions

--- src/root_dir.rs
+++ src/root_dir.rs
@@ -1,5 +1,5 @@
-
-use std::path::{PathBuf};
+use std::fs::{read_dir, DirEntry};
+use std::path::{Path, PathBuf};
 
 pub struct MusicDir {
     pub path: PathBuf,
@@ -10,15 +10,42 @@ pub struct MusicDir {
 pub struct RootDir {
     root: MusicDir
 }
- impl RootDir {
-     pub fn new(root: PathBuf) -> Self {
-         let music_dir = MusicDir {
-             path: root,
-             sub_dirs: vec![],
-             tracks: vec![],
-         };
-         Self {
-             root: music_dir,
-         }
-     }
- }
+
+
+fn get_mp3s(path: &Path) -> Option<Vec<PathBuf>> {
+    let mut res = vec![];
+    let read_dir = read_dir(path).ok()?;
+
+    for entry in read_dir.flatten() {
+        let path_buf = entry.path();
+        if let Some(ext) = path_buf.extension() {
+            if ext == "mp3" {
+                res.push(path_buf);
+            }
+        }
+    }
+
+    if res.is_empty() {
+        None
+    } else {
+        Some(res)
+    }
+}
+
+fn get_sub_dirs(path: &Path) -> Option<Vec<PathBuf>> {
+    let mut res = vec![];
+    let read_dir = read_dir(path).ok()?;
+
+    for entry in read_dir.flatten() {
+        let path_buf = entry.path();
+        if path_buf.is_dir() {
+            res.push(path_buf);
+        }
+    }
+
+    if res.is_empty() {
+        None
+    } else {
+        Some(res)
+    }
+}
