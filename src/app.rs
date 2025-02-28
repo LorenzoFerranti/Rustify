8a1f7aa2b332255168dd681f819deda78a266980
Lorenzo Ferranti <lorenzoferranti04@outlook.it>
2025-02-28 11:44:31 +0100

    reading and writing json data

--- src/app.rs
+++ src/app.rs
@@ -1,21 +1,31 @@
-use crate::helper::formatted_duration;
-use crate::music_player::MusicPlayer;
-
+use std::fs::File;
+use std::io::Write;
+use std::thread;
 use std::path::PathBuf;
-use std::thread::sleep;
 use std::time::Duration;
 
-use crate::root_music_dir::RootMusicDir;
 use eframe::egui::{
     Align, Button, CentralPanel, Color32, Context, Image, Layout, RichText, Slider, TextEdit,
     TextStyle, TextureOptions, TopBottomPanel, Ui,
 };
 use eframe::{CreationContext, Frame};
 
-const MY_LOCAL_PATH: &str = "C:\\Users\\loren\\Desktop\\OSTs";
+use serde::{Serialize, Deserialize};
+
+use crate::helper;
+use crate::music_player::MusicPlayer;
+use crate::root_music_dir::RootMusicDir;
+
+const DATA_RELATIVE_PATH: &str = "data.json";
+
+#[derive(Default, Debug, Serialize, Deserialize)]
+struct RustifyData {
+    root_path: String,
+    volume: f32,
+}
 
 pub struct RustifyApp {
-    path: String,
+    root_path: String,
     music_player: MusicPlayer,
     volume_input: f32,
     duration_slider: f32,
@@ -25,17 +35,45 @@ impl RustifyApp {
     pub fn new(cc: &CreationContext) -> Self {
         // repaint thread
         let ctx_clone = cc.egui_ctx.clone();
-        std::thread::spawn(move || loop {
-            sleep(Duration::from_millis(500));
+        thread::spawn(move || loop {
+            thread::sleep(Duration::from_millis(500));
             ctx_clone.request_repaint();
         });
 
+        // read data
+        let data = match Self::get_data() {
+            None => {
+                let new_data = RustifyData::default();
+                Self::write_data(&new_data);
+                new_data
+            }
+            Some(data) => {
+                data
+            }
+        };
+
         Self {
-            path: MY_LOCAL_PATH.to_string(),
+            root_path: data.root_path,
             music_player: MusicPlayer::new(),
-            volume_input: 1.0,
+            volume_input: data.volume,
             duration_slider: 0.0,
         }
+    }
+
+    fn get_data() -> Option<RustifyData> {
+        let file = File::open(DATA_RELATIVE_PATH).ok()?;
+        let data : RustifyData = serde_json::from_reader(&file).ok()?;
+        Some(data)
+    }
+
+    fn write_data(data: &RustifyData) {
+        let json_string = serde_json::to_string(data).unwrap();
+        let mut file = File::create(DATA_RELATIVE_PATH).unwrap();
+        file.write((&json_string).as_ref()).expect("Failed to write file");
+    }
+
+    fn save_data(&self) {
+        Self::write_data(&RustifyData{ root_path: self.root_path.clone(), volume: self.volume_input })
     }
 
     fn spawn_duration_slider(&mut self, ui: &mut Ui) {
@@ -106,13 +144,13 @@ impl eframe::App for RustifyApp {
 
                 // slider
                 ui.horizontal(|ui| {
-                    ui.label(formatted_duration(
+                    ui.label(helper::formatted_duration(
                         &self.music_player.get_current_track_pos(),
                     ));
                     // layout needed for correct expansion of the slider
                     ui.with_layout(Layout::right_to_left(Align::Center), |ui| {
                         if let Some(track) = self.music_player.get_current_track() {
-                            ui.label(formatted_duration(&track.duration));
+                            ui.label(helper::formatted_duration(&track.duration));
                         } else {
                             ui.label("00:00");
                         }
@@ -132,6 +170,9 @@ impl eframe::App for RustifyApp {
                             if response.changed() {
                                 self.music_player.set_volume(self.volume_input);
                             }
+                            if response.drag_stopped() {
+                                self.save_data();
+                            }
                         });
                     });
                     cols[1].vertical_centered(|ui| self.spawn_pause_button(ui));
@@ -145,13 +186,14 @@ impl eframe::App for RustifyApp {
         CentralPanel::default().show(ctx, |ui| {
             ui.label("Root path");
             ui.add(
-                TextEdit::singleline(&mut self.path)
+                TextEdit::singleline(&mut self.root_path)
                     .desired_width(f32::INFINITY)
                     .font(TextStyle::Monospace),
             );
             if ui.button("Play root").clicked() {
+                self.save_data();
                 self.music_player
-                    .set_playlist(RootMusicDir::new(PathBuf::from(self.path.clone())));
+                    .set_playlist(RootMusicDir::new(PathBuf::from(self.root_path.clone())));
             }
             ui.add_space(15.0);
 
